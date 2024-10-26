79d369acd0f965a6d2a2b7e22cef6f4b66876e1a
Evanition <dewaynebarnes123@gmail.com>
2024-10-26 15:10:24 -0400

    pushing changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Profile from './components/Profile';
+import Dashboard from './components/Dashboard';
+import Bills from './components/Bills';
+import Friends from './components/Friends';
+import Camera from './components/Camera';
 
 function App() {
+  const [user, setUser] = useState(null); // Manages user state
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar user={user} setUser={setUser} /> {/* Include Navbar */}
+      <Routes>
+        <Route path="/" element={<Home user={user} />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard user={user} />} />
+        <Route path="/profile" element={<Profile user={user} />} />
+        <Route path="/bills" element={<Bills />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/camera" element={<Camera />} />
+
+      </Routes>
     </div>
   );
 }
